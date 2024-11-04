78dc4a8043ee1e34408cbe94cce001aa54c03d3f
leonohub <mendoncal717@gmail.com>
2024-11-04 16:57:34 -0300

    Added the API logic
    Added the read of the API

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 async function searchCharacter() {
     const name = document.getElementById('name').value;
-    console.log(name);
+
+    const response = await fetch(`https://rickandmortyapi.com/api/character/?name=${name}`);
+    const data = await response.json();
+            
+
+    if (data.results && data.results.length > 0) {
+        const character = data.results[0]; // get the first character
+        console.log(character);
+    }
 }
